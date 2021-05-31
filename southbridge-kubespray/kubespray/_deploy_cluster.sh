b9cf71e4721d8807e3c835ffc094e030628b2857
Babkin <babkin@reg.ru>
2021-06-01 00:20:04 +0600

    add kubespray-southbridge

---
+++ southbridge-kubespray/kubespray/_deploy_cluster.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+###ssh-agent bash
+#ssh-add ~/.ssh/id_rsa
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 adminname"
+  exit 1
+fi
+
+d=$(date '+%Y.%m.%d_%H:%M')
+
+# if ssh access to servers only with password, then
+# install sshpass on all nodes and add `-k` options 
+
+export ANSIBLE_LOG_PATH=./deploy-$d.log
+ansible-playbook -u "$1" $K -i inventory/s000/inventory.ini cluster.yml -b --diff --ask-pass
